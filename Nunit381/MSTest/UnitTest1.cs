b9abe3e411f53bac6a8ef63076fb129b254d1756
Teodora P Stanev <teodoras@microsoft.com>
2021-10-12 19:57:50 +0000

    Updated UnitTest1.cs

--- Nunit381/MSTest/UnitTest1.cs
+++ Nunit381/MSTest/UnitTest1.cs
@@ -9,5 +9,45 @@ namespace MSTest
         public void TestMethod1()
         {
         }
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+        }
+        [TestMethod]
+        public void TestMethod8()
+        {
+        }
+        [TestMethod]
+        public void TestMethod11()
+        {
+        }
+        [TestMethod]
+        public void TestMethod21()
+        {
+        }
+        [TestMethod]
+        public void TestMethod31()
+        {
+        }
     }
 }
